31e5318c15a0591941dda4a0837bbdbb2cdf06af
Jonathan Kaczynski <jmkacz@gmail.com>
2021-12-16 00:09:38 -0500

    Solution for 2021 day 15 (networkx)

--- 2021/day15/python/part1.py
+++ 2021/day15/python/part1.py
@@ -1,3 +1,4 @@
+import networkx as nx  # type: ignore
 from typing import List
 
 
@@ -8,16 +9,51 @@ def parse(lines: List[str]) -> List[List[int]]:
     return result
 
 
-def compute_answer(lines: List[str]) -> int:
-    risk = parse(lines)
-    rc = len(risk)
-    cc = len(risk[0])
+def original(lines: List[str]) -> int:
+    """NOTE: This only works because the solution is strictly down and right!"""
+    risks = parse(lines)
+    rc = len(risks)
+    cc = len(risks[0])
     for r in range(rc):
         for c in range(cc):
             if r == 0 and c > 0:
-                risk[r][c] += risk[r][c - 1]
+                risks[r][c] += risks[r][c - 1]
             if r > 0 and c == 0:
-                risk[r][c] += risk[r - 1][c]
+                risks[r][c] += risks[r - 1][c]
             if r > 0 and c > 0:
-                risk[r][c] += min(risk[r][c - 1], risk[r - 1][c])
-    return risk[rc - 1][cc - 1] - risk[0][0]
+                risks[r][c] += min(risks[r][c - 1], risks[r - 1][c])
+    return risks[rc - 1][cc - 1] - risks[0][0]
+
+
+def build_graph(risks: List[List[int]]) -> nx.Graph:
+    graph = nx.DiGraph()
+    rc = len(risks)
+    cc = len(risks[0])
+    for r in range(rc):
+        for c in range(cc):
+            graph.add_node((r, c))
+            for dr, dc in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+                r2, c2 = r + dr, c + dc
+                if 0 <= r2 < rc and 0 <= c2 < cc:
+                    graph.add_edge((r, c), (r2, c2), weight=risks[r2][c2])
+    # print(graph)
+    # print(graph.nodes)
+    # print(graph.edges(data=True))
+    return graph
+
+
+def alternative(lines: List[str]) -> int:
+    risks = parse(lines)
+    rc = len(risks)
+    cc = len(risks[0])
+    graph = build_graph(risks)
+    path = nx.shortest_path(
+        graph, source=(0, 0), target=(rc - 1, cc - 1), weight="weight"
+    )
+    # print(path)
+    return sum(risks[r][c] for r, c in path[1:])
+
+
+def compute_answer(lines: List[str]) -> int:
+    return original(lines)
+    # return alternative(lines)
--- 2021/day15/python/part2.py
+++ 2021/day15/python/part2.py
@@ -1,4 +1,5 @@
 import heapq
+import networkx as nx  # type: ignore
 import sys
 from typing import List, Tuple
 
@@ -20,20 +21,54 @@ def parse(lines: List[str]) -> List[List[int]]:
     return result
 
 
-def compute_answer(lines: List[str]) -> int:
+def original(lines: List[str]) -> int:
     """dijkstras single source shortest path algorithm"""
-    weights = parse(lines)
-    rc = len(weights)
-    cc = len(weights[0])
-    risks = [[sys.maxsize // 4] * cc for _ in range(rc)]
+    risks = parse(lines)
+    rc = len(risks)
+    cc = len(risks[0])
+    graph = [[sys.maxsize // 4] * cc for _ in range(rc)]
     pq: List[Tuple[int, Tuple[int, int]]] = []
     heapq.heappush(pq, (0, (0, 0)))
-    risks[0][0] = 0
+    graph[0][0] = 0
     while pq:
-        weight, (r, c) = heapq.heappop(pq)
+        risk, (r, c) = heapq.heappop(pq)
         for dr, dc in ((-1, 0), (1, 0), (0, -1), (0, 1)):
             if 0 <= r + dr < rc and 0 <= c + dc < cc:
-                if risks[r + dr][c + dc] > risks[r][c] + weights[r + dr][c + dc]:
-                    risks[r + dr][c + dc] = risks[r][c] + weights[r + dr][c + dc]
-                    heapq.heappush(pq, (risks[r + dr][c + dc], (r + dr, c + dc)))
-    return risks[rc - 1][cc - 1]
+                if graph[r + dr][c + dc] > graph[r][c] + risks[r + dr][c + dc]:
+                    graph[r + dr][c + dc] = graph[r][c] + risks[r + dr][c + dc]
+                    heapq.heappush(pq, (graph[r + dr][c + dc], (r + dr, c + dc)))
+    return graph[rc - 1][cc - 1]
+
+
+def build_graph(risks: List[List[int]]) -> nx.Graph:
+    graph = nx.DiGraph()
+    rc = len(risks)
+    cc = len(risks[0])
+    for r in range(rc):
+        for c in range(cc):
+            graph.add_node((r, c))
+            for dr, dc in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+                r2, c2 = r + dr, c + dc
+                if 0 <= r2 < rc and 0 <= c2 < cc:
+                    graph.add_edge((r, c), (r2, c2), weight=risks[r2][c2])
+    # print(graph)
+    # print(graph.nodes)
+    # print(graph.edges(data=True))
+    return graph
+
+
+def alternative(lines: List[str]) -> int:
+    risks = parse(lines)
+    rc = len(risks)
+    cc = len(risks[0])
+    graph = build_graph(risks)
+    path = nx.shortest_path(
+        graph, source=(0, 0), target=(rc - 1, cc - 1), weight="weight"
+    )
+    # print(path)
+    return sum(risks[r][c] for r, c in path[1:])
+
+
+def compute_answer(lines: List[str]) -> int:
+    return original(lines)
+    # return alternative(lines)
